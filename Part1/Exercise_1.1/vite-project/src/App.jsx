d256761cde87b86f1eef78ee95b790f14f0295ef
Alejandro Martínez <martinezalejandrouy7@gmail.com>
2024-03-04 01:31:52 -0300

    Finishing Part1-c

--- Part1/Exercise_1.1/vite-project/src/App.jsx
+++ Part1/Exercise_1.1/vite-project/src/App.jsx
@@ -1,63 +1,22 @@
+import { useState } from 'react'
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [ counter, setCounter ] = useState(0)
 
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaserByOne = () => setCounter(counter - 1)
+  const setZero = () => setCounter(0)
+
+  const Display = ({counter}) => <div>{counter}</div>
+
+  const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>
+  
   return (
     <div>
-      <Header course={course.name}/>
-      <Content part1={course.parts[0].name} exercises1={course.parts[0].exercises}/>
-      <Content part2={course.parts[1].name} exercises2={course.parts[1].exercises}/>
-      <Content part3={course.parts[2].name} exercises3={course.parts[2].exercises}/>
-      <Total exercises1={course.parts[0].name} exercises2={course.parts[1].name} exercises3={course.parts[2].name}/>
-    </div>
-  )
-}
-
-const Header = (props) => {
-  return (
-    <div>
-      <p>Course name: {props.course}</p>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part name={props.part1} exercises={props.exercises1}/>
-      <Part name={props.part2} exercises={props.exercises2}/>
-      <Part name={props.part3} exercises={props.exercises3}/>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.name} {props.exercises}</p>
-    </div>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises: {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <Display counter={counter}/>
+      <Button onSmash={increaseByOne} text="Plus"/>
+      <Button onSmash={decreaserByOne} text="Minus"/>
+      <Button onSmash={setZero} text="Zero"/>
     </div>
   )
 }
