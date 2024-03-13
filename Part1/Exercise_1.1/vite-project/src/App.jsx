12503009974de4329bb1a03de4f62edd5f16f667
Alejandro Martínez <martinezalejandrouy7@gmail.com>
2024-03-13 14:10:25 -0300

    Starting Exercises

--- Part1/Exercise_1.1/vite-project/src/App.jsx
+++ Part1/Exercise_1.1/vite-project/src/App.jsx
@@ -1,22 +1,58 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaserByOne = () => setCounter(counter - 1)
-  const setZero = () => setCounter(0)
+  const History = (props) => {
 
-  const Display = ({counter}) => <div>{counter}</div>
+    if (props.allClicks.length === 0) {
+      return (
+        <div>
+          <p>The app is used by pressing the buttons</p>
+        </div>
+      )
+    }
 
-  const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>
-  
+    return (
+      <div>
+        <p>
+          Button press history: {props.allClicks.join(' ')}
+        </p>
+      </div>
+    )
+  }
+
+  const Button = ({handleClick, text}) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updateLeft = left + 1
+    setLeft(updateLeft)
+    setTotal(right + updateLeft)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updateRight = right + 1
+    setRight(updateRight)
+    setTotal(updateRight + left)
+  }
+
   return (
     <div>
-      <Display counter={counter}/>
-      <Button onSmash={increaseByOne} text="Plus"/>
-      <Button onSmash={decreaserByOne} text="Minus"/>
-      <Button onSmash={setZero} text="Zero"/>
+      {left}
+      <Button handleClick={handleLeftClick} text='Left'/>
+      <Button handleClick={handleRightClick} text='Right'/>
+      {right}
+      <p>Total: {total}</p>
+      <History allClicks={allClicks}/> 
     </div>
   )
 }
